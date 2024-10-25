16deffb7f053ccecac889bb29d8ef45b8fe74678
IkasimDev <misakihayatop@hotmail.com>
2024-10-25 16:51:52 -0300

    Criação Header

---
+++ src/components/Header/header.js
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <header className="w-full h-auto flex items-center justify-center p-4">
+            <div id="lexigoT">
+                <h1 className="text-4xl font-bold">LEXIGO</h1>
+            </div>
+        </header>
+    );
+}
